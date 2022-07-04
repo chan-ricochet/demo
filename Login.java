348b4d43e819a16d41520d70006da84cd8378c55
chan-ricochet <chandrikamohancs@gmail.com>
2022-07-04 17:20:39 +0530

    Edited Login via ram and shyam

--- Login.java
+++ Login.java
@@ -1,3 +1,13 @@
 public class Login
 {
+	public void SingleSignIn()
+	{
+		System.out.println("GMail");
+		System.out.println("Yahoo");
+	}
+
+	public void SignInOTP()
+	{
+		System.out.println("OTP");
+	}
 }
