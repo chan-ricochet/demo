09c4998574dfed5459f40f57b3afe041a7992e02
chan-ricochet <chandrikamohancs@gmail.com>
2022-07-04 13:15:09 +0530

    Added if condition

--- abc.java
+++ abc.java
@@ -1,7 +1,9 @@
 public class abc
 {
-public static void main(String[] args)
-{
-System.out.println(" Hello everybody!");
-}
+	public static void main(String[] args)
+	{
+		int a=5,b=10;
+		if(a+b>10)
+			System.out.println("HI everybody!");
+	}
 }
